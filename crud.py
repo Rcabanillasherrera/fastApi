6e9d17941f6f57965bfd4714372681e39ee22352
Rcabanillasherrera <joelherrera4898@gmail.com>
2024-11-08 19:45:20 -0500

    nueva conexion a postgre

--- crud.py
+++ crud.py
@@ -1,5 +1,7 @@
 # crud.py
 from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 from models import Cliente
 from schemas import ClienteCreate
 
@@ -44,3 +46,84 @@ def delete_cliente(db: Session, cliente_id: int):
         db.delete(db_cliente)
         db.commit()
     return db_cliente
+
+
+#funciones PostgreSQL
+
+async def get_cliente_postgresql(db: AsyncSession, cliente_id: int):
+
+  result = await db.execute(select(Cliente).filter(Cliente.id == cliente_id))
+
+  return result.scalar_one_or_none()
+
+
+
+async def get_clientes_postgresql(db: AsyncSession, skip: int = 0, limit: int = 10):
+
+  result = await db.execute(select(Cliente).offset(skip).limit(limit))
+
+  return result.scalars().all()
+
+
+
+async def create_cliente_postgresql(db: AsyncSession, cliente: ClienteCreate):
+
+  db_cliente = Cliente(
+
+    nombre=cliente.nombre,
+
+    direccion=cliente.direccion,
+
+    telefono=cliente.telefono,
+
+    email=cliente.email
+
+  )
+
+  db.add(db_cliente)
+
+  await db.commit()
+
+  await db.refresh(db_cliente)
+
+  return db_cliente
+
+
+
+async def update_cliente_postgresql(db: AsyncSession, cliente_id: int, cliente: ClienteCreate):
+
+  db_cliente = await get_cliente_postgresql(db, cliente_id)
+
+  if db_cliente:
+
+    db_cliente.nombre = cliente.nombre
+
+    db_cliente.direccion = cliente.direccion
+
+    db_cliente.telefono = cliente.telefono
+
+    db_cliente.email = cliente.email
+
+    await db.commit()
+
+    await db.refresh(db_cliente)
+
+  return db_cliente
+
+
+
+async def delete_cliente_postgresql(db: AsyncSession, cliente_id: int):
+
+  db_cliente = await get_cliente_postgresql(db, cliente_id)
+
+  if db_cliente:
+
+    await db.delete(db_cliente)
+
+    await db.commit()
+
+  return db_cliente
+
+
+
+#end funciones PostgreSQL
--- database.py
+++ database.py
@@ -1,13 +1,22 @@
 # database.py
 from sqlalchemy import create_engine, text
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base #MYSQL
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine #POSTGRESQL
 from sqlalchemy.orm import sessionmaker
 
 # Reemplaza 'username' con tu usuario de MySQL y 'mi_base_datos' con el nombre de tu base de datos
-DATABASE_URL = "mysql+pymysql://root@127.0.0.1:3306/fastapidb"
+MYSQL_DATABASE_URL = "postgresql+asyncpg://postgres:1234@localhost:5432/ahorroperu"
+POSTGRESQL_DATABASE_URL = "postgresql+asyncpg://postgres:1234@localhost:5432/dbpy"
 
-engine = create_engine(DATABASE_URL)
+#MOTOR MYSQL
+engine = create_engine(POSTGRESQL_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+#MOTOR DE POSTGRESQL
+postgresql_engine = create_async_engine(POSTGRESQL_DATABASE_URL, echo=True)
+PostgreSQLSessionLocal = sessionmaker(autocommit=False, autoflush=False, bin=postgresql_engine, class_=AsyncSession)
+
+#DECLARACION DE BASE DE DATOS
 Base = declarative_base()
 
 
